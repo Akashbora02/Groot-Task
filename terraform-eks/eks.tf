b65758186a64ec2af0a86ca168e96f6e6ad2d70e
Akashbora02 <akashbora0082.com>
2026-05-19 15:05:09 +0530

    task updated

--- terraform-eks/eks.tf
+++ terraform-eks/eks.tf
@@ -7,7 +7,8 @@ module "eks" {
 
   enable_cluster_creator_admin_permissions = true
   cluster_endpoint_public_access = true
-
+  cluster_endpoint_private_access = true
+  
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
